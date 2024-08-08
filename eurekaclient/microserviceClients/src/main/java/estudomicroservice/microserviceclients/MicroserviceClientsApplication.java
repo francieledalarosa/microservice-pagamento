d27447e6a7f4643cb7a122b65ccf635612ad708d
Franciele Dalarosa <140400674+francieledalarosa@users.noreply.github.com>
2024-08-08 15:28:58 -0300

    feat: conectando com o eurekaserver

--- eurekaclient/microserviceClients/src/main/java/estudomicroservice/microserviceclients/MicroserviceClientsApplication.java
+++ eurekaclient/microserviceClients/src/main/java/estudomicroservice/microserviceclients/MicroserviceClientsApplication.java
@@ -2,8 +2,10 @@ package estudomicroservice.microserviceclients;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroserviceClientsApplication {
 
     public static void main(String[] args) {
