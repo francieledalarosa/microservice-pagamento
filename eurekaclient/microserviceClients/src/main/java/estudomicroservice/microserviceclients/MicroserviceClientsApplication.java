53550842ce4b0b1c3c15c2faab8debaf931c2d61
Franciele Dalarosa <140400674+francieledalarosa@users.noreply.github.com>
2024-08-08 15:07:26 -0300

    feat: preparação do pom

---
+++ eurekaclient/microserviceClients/src/main/java/estudomicroservice/microserviceclients/MicroserviceClientsApplication.java
@@ -0,0 +1,13 @@
+package estudomicroservice.microserviceclients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceClientsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceClientsApplication.class, args);
+    }
+
+}
