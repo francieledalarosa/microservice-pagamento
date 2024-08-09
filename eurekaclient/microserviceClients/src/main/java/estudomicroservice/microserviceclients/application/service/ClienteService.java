ee246cba9c32ee76506ff201783a0c22e7d5c398
Franciele Dalarosa <140400674+francieledalarosa@users.noreply.github.com>
2024-08-09 08:23:45 -0300

    Create ClienteService.java

---
+++ eurekaclient/microserviceClients/src/main/java/estudomicroservice/microserviceclients/application/service/ClienteService.java
@@ -0,0 +1,26 @@
+package estudomicroservice.microserviceclients.application.service;
+
+import estudomicroservice.microserviceclients.domain.Cliente;
+import estudomicroservice.microserviceclients.infra.repository.ClienteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteService {
+
+    private final ClienteRepository repository;
+
+    @Transactional
+    public Cliente save(Cliente cliente){
+        return repository.save(cliente);
+    }
+
+    public Optional<Cliente> getByCPF(String cpf){
+        return repository.findByCpf(cpf);
+    }
+}
+
